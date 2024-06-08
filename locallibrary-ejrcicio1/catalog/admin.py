73583d0128f819d4502aa6312028bc9e76276f3a
Eduardo Cuno <ecunos@unsa.edu.pe>
2024-06-08 17:06:51 -0500

    Parte 4 del tutorial configurando el sitio de administracion de DJANJO

--- locallibrary-ejrcicio1/catalog/admin.py
+++ locallibrary-ejrcicio1/catalog/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+class BooksInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    # Define los campos que se mostrarán en la vista de detalle
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+    
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+    list_filter = ('genre',)  # Agrega filtros para el género
+
+    def display_genre(self, obj):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in obj.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    search_fields = ['book__title', 'id']  # Esto permite buscar por título de libro y por ID de instancia de libro
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }), 
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
 admin.site.register(Language)
