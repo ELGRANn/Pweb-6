26bcb264ba10f92e857b24eaf5b5cf25feff4c23
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-08 20:11:16 -0500

    Se termino la parte 7 , Se creo un contador para visualizar el nmero de veces que se visita la pagina

--- locallibrary-ejrcicio1/catalog/views.py
+++ locallibrary-ejrcicio1/catalog/views.py
@@ -28,10 +28,22 @@ def index(request):
         'num_genres': num_genres,
         'num_books_with_word': num_books_with_word,
     }
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books':num_books,
+        'num_instances':num_instances,
+        'num_instances_available':num_instances_available,
+        'num_authors':num_authors,
+        'num_visits':num_visits,
+    }
+
+
     # Renderizar la plantilla HTML index.html con los datos en la variable de contexto
     return render(request, 'index.html', context=context)
 from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = 10    
