067c6f18a020a01741b72310104bf4266ea6e56d
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-06-14 10:37:36 -0500

    mejorando los componentes de los modelos

--- laboratorio_07/laboratorio_7/notas/models.py
+++ laboratorio_07/laboratorio_7/notas/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Alumno(models.Model):
-    CUI = models.IntegerField()
-    nombres = models.TextField()
-    apellidos = models.TextField()
+    CUI = models.IntegerField()#(max_digits=8)
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
 
 class Curso(models.Model):
-    id_curso = models.IntegerField()
-    nombre_curso = models.TextField()
+    id_curso = models.IntegerField()#(max_digits=5)
+    nombre_curso = models.CharField(max_length=100)
 
 class NotasAlumnosPorCurso(models.Model):
-    cui_alumno = models.IntegerField()
-    id_curso = models.IntegerField()
-    nota = models.IntegerField()
+    cui_alumno = models.IntegerField()#(max_digits=8)
+    id_curso = models.IntegerField()#(max_digits=5)
+    nota = models.IntegerField()#(max_digits=2)
     
