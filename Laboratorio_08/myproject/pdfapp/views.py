38688d9634c57e52da3dbffa4c00909925af4850
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-24 20:15:09 -0500

    Se agregaron las url y se creo la vista

--- Laboratorio_08/myproject/pdfapp/views.py
+++ Laboratorio_08/myproject/pdfapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
+from weasyprint import HTML
 
-# Create your views here.
+def generate_pdf(request):
+    template = get_template('pdfapp/sample_template.html')
+    context = {
+        'title': 'Rendirizador de PDF',
+        'content': 'PDF generado desde HTML',
+    }
+    html_content = template.render(context)
+    pdf_file = HTML(string=html_content).write_pdf()
+
+    response = HttpResponse(pdf_file, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="sample.pdf"'
+    return response
